3f5d649facd6dc2bccb94cc6d23844cbff247a59
Joe Archer <joe.archer@gmail.com>
2014-07-02 08:31:04 +0100

    removed constructor that wasn't working

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -2,10 +2,5 @@
 
 class ControllerBase extends \Phalcon\Mvc\Controller
 {
-	function __construct(){
-		parent::__construct();
-		$config   = dirname(__DIR__) . '/libraries/hybridauth/config.php';
-  		require_once( "hybridauth/Hybrid/Auth.php" );
-		$this->hybridauth = new Hybrid_Auth( $config );
-	}
+	
 }
